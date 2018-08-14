0d76a02c3910645153725ddf4b20b04d285a242e
Carnitas <andres.huerta.14@sansano.usm.cl>
2018-08-14 16:15:07 -0300

    agregada solucion inicial

--- src/MC.cpp
+++ src/MC.cpp
@@ -4,14 +4,19 @@
 #include <vector>
 #include <sstream>
 #include <map>
+#include <algorithm>
 
 #include "nodo.h"
 #include "camion.h"
 #include "leche.h"
 
 std::vector<Camion> camiones;
+std::vector<Nodo> planta;
 std::vector<Nodo> nodos;
 std::vector<Leche> leches;
+std::vector<std::vector<Nodo>> mejorSolucion;
+std::vector<std::vector<Nodo>> solucionInicial;
+int totalCamiones;
 
 void readFile(std::string arch) {
     std::ifstream file(arch);
@@ -23,6 +28,7 @@ void readFile(std::string arch) {
 
     file >> word;
     n = std::stoi(word);
+    totalCamiones = n;
     while ( i < n && file >> word ) {
         Camion camioncito = Camion(std::stoi(word));
         camiones.push_back(camioncito);
@@ -44,7 +50,7 @@ void readFile(std::string arch) {
     i = 0;
     file >> word;
     n = std::stoi(word);
-    while ( i < n && file >> word ) {
+    while (i < n && file >> word ) {
         std::vector<std::string> datos;
         datos.clear();
         datos.push_back(word);
@@ -52,11 +58,41 @@ void readFile(std::string arch) {
             file >> word;
             datos.push_back(word);
         }
-        std::cout << datos[0] << "   " << datos[1] << "   "  << datos[2] << "   "  << datos[3] << "   "  << datos[4] << "   "  << "\n";
         Nodo nodito = Nodo( std::stoi(datos[0]), std::stof(datos[1]), std::stof(datos[2]), datos[3][0], std::stoi(datos[4]));
-        nodos.push_back(nodito);
+        if ( i != 0 ) {
+            nodos.push_back(nodito);
+        }
+        else {
+            planta.push_back(nodito);
+        }
         i++;
     }
+}
+
+void solucionRandom() {
+    std::vector<Nodo> borrable = nodos;
+    std::random_shuffle ( borrable.begin(), borrable.end() );
+    int n = borrable.size();
+    for ( int i = 0; i < totalCamiones; i++ ) {
+        std::vector<Nodo> lista;
+        solucionInicial.push_back(lista);
+    }
+    for ( int i = 0; i < n; i++ ) {
+        solucionInicial[i%totalCamiones].push_back(borrable[i]);
+    }
+}
+
+float calidadSolucion() {
+
+}
+
+float calcularDistancia(Nodo a, Nodo b) {
+    float i = a.x - b.x;
+    float j = a.y - b.y;
+    i = i*i;
+    j = j*j;
+    return sqrt(i+j);
+
 }
 
 int main(int argc, char* argv[]) {
@@ -65,8 +101,8 @@ int main(int argc, char* argv[]) {
         return -1;
         }
 
-
     readFile(argv[1]);
-
+    solucionRandom();
+    
     return 0;
 }
